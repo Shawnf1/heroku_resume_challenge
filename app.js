b446c3b85e6f7f753d9672c3907738d941316da8
TShaunik <shawnf1@gmail.com>
2015-08-25 15:12:47 -0500

    Add public folder with resume .html, .css, .js

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 /**
  * Created by Shawn on 8/25/15.
  */
-var http = require('http');
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
 
-http.createServer(function (req, res) {
-	res.writeHead(200);
-	res.write('Hello world!');
-	res.end();
-}).listen(process.env.PORT || 3000);
+app.get('/', function (req, res) {
+	//res.send('Hello express world!');
+});
+
+var server = app.listen(3000, function () {
+	var port = server.address().port;
+
+	//console.log("Testing listening on port ", port);
+});
+
+
+//var http = require('http');
+//
+//http.createServer(function (req, res) {
+//	res.writeHead(200);
+//	res.write('Hello world!');
+//	res.end();
+//}).listen(process.env.PORT || 3000);
